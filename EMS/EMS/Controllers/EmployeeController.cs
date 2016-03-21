d5a297dd9aef79a8c203a547292d8f9587ea3201
Ankita Gupta <angupta@greenapplesolutions.com>
2016-03-21 13:27:21 +0530

    Employee-Controller Add- view

---
+++ EMS/EMS/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EMS.Models;
+
+namespace EMS.Controllers {
+    public class EmployeeController : Controller {
+        // GET: Employee
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult Add() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(EmployeeViewModels model) {
+
+            return View();
+        }
+    }
+}
